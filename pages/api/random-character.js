ecf7c73820d5693e63db9ae2be4e1280344ece6a
Alexander-Kolotow <147614805+Alexander-Kolotow@users.noreply.github.com>
2023-11-30 08:27:23 +0000

    create the api and random character

---
+++ pages/api/random-character.js
@@ -0,0 +1,23 @@
+import Chance from "chance";
+
+const chance = new Chance();
+
+export default function handleRandomCharacter(request, response) {
+
+  if (request.url === '/api/random-character') {
+    const randomCharacter = {
+      firstName: chance.first(),
+      lastName: chance.last(),
+      gender: chance.gender(),
+      age: chance.age(),
+      email: chance.email(),
+      twitter: `@${chance.twitter()}`,
+      geohash: chance.geohash(),
+    };
+
+    response.status(200).json(randomCharacter);
+  } else {
+    response.status(404).end("Page not found");
+  }
+};
+
